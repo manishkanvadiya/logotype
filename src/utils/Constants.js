df6f1d5f7807ff73eb70bb51fa74e79e7d023b74
manishkanvadiya <manishkanvadiya@gmail.com>
2024-08-08 11:19:59 +0530

    dsfd

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -15,15 +15,8 @@ export const googleLoginApi = "/v1/user/signUpGoogle";
 
 //Google drive
 
-// export const CLIENT_ID =
-//   "630737192676-470r9qhp8s5fauvg7d8apcpndpc432as.apps.googleusercontent.com";
-
-
-export const CLIENT_ID =
-  "878062054698-pc5s342mc9kecngkq3u9rnhcaa9chmj2.apps.googleusercontent.com";
-// export const API_KEY = "AIzaSyCVsRf6hYy67FdgEYkFGmj97xHYViCD2Bk";
-// export const API_KEY = "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";
-export const API_KEY = "AIzaSyAQjuXONF86tr-KAwIP8S2ET6kKWdx4b7I";
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+export const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const DISCOVERY_DOCS = [
   "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
